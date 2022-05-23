94375e35511494ce18726e1f711f01ec78c8bb79
wadafacc <70700633+Wadafacc@users.noreply.github.com>
2022-05-23 16:17:20 +0200

    updated landing page

--- cultus-client/components/Front.vue
+++ cultus-client/components/Front.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
     <div class="lines"></div>
+    <div class="grid"></div>
     <div class="logo">
       <img src="~/static/toilet.gif" alt="" width="100%" />
       <div class="text">
@@ -9,8 +10,11 @@
           <span>cultus</span>
         </h3>
       </div>
-      <h4>amazing.</h4>
+      <h4>best toilets.</h4>
     </div>
+    <a href="" class="link">
+      <span>Our Products</span>
+    </a>
   </div>
 </template>
 
