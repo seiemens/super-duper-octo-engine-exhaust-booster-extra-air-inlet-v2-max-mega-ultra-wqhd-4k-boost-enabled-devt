e9df276a1dae579cb25b4ec4f3bcaa95f1e11591
wadafacc <70700633+Wadafacc@users.noreply.github.com>
2022-05-23 10:44:06 +0200

    added main page

---
+++ cultus-client/components/Front.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <div class="lines"></div>
+    <div class="logo">
+      <img src="~/static/toilet.gif" alt="" width="100%" />
+      <div class="text">
+        <h3 id="name">
+          <span>cultus</span>
+          <span>cultus</span>
+        </h3>
+      </div>
+      <h4>amazing.</h4>
+    </div>
+  </div>
+</template>
+
+<style src="~/assets/styles/front.css">
+</style>
